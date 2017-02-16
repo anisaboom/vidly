6846a7a8e06c90b40c8684a0d62c9dce40b4d32d
anisaboom <al318@greenwich.ac.uk>
2017-02-16 19:44:02 +0000

    changing the model

--- vidly/Models/Customer.cs
+++ vidly/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; } //navigation property, useful when you want to load object like customer and membership together
+        public byte MembershipTypeId { get; set; }
     }
 }
